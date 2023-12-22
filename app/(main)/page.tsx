9cebd814276ca3484a435bebf7d4781b6646e6cd
csachdeva83 <cherishsachdeva16@gmail.com>
2023-12-22 18:03:44 +0530

    Fetch data from API server side

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,47 @@
-const MainPage = () => {
+interface IUser {
+    id: string;
+    name: string;
+    available: boolean;
+}
+
+interface ITicket {
+    id: string;
+    title: string;
+    tag: string[];
+    userId: string;
+    status: string;
+    priority: number;
+
+}
+
+type TSupport = {
+    tickets: ITicket[]
+    users: IUser[]
+}
+
+export const getData = async (): Promise<TSupport> => {
+    const res = await fetch(process.env.TICKETS_AND_USERS_URL as string, {
+        next: {
+            revalidate: 10 
+        }
+    });
+    const data: TSupport = await res.json();
+    return data;
+};
+
+
+
+const MainPage = async () => {
+
+    const data = await getData();
+
     return (
-        <div className="border-2 border-green-600 h-full">
-            Hello
+        <div>
+            {
+                data.tickets.map((ticket: ITicket) => (
+                    <div key={ticket.id}>{ticket.title}</div>
+                ))
+            }
         </div>
     );
 }
